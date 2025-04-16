5e58e6ab0dbd664102348ac74c60778b5ebca0ee
woohyeon <woohyeon6885@gmail.com>
2025-04-16 15:26:23 +0900

    feat: MemberNotFoundException에서 username을 받는 생성자 추가

--- src/main/java/hong/postService/exception/member/MemberNotFoundException.java
+++ src/main/java/hong/postService/exception/member/MemberNotFoundException.java
@@ -5,4 +5,8 @@ public class MemberNotFoundException extends RuntimeException {
     public MemberNotFoundException(Long id) {
         super("해당 Id의 회원이 존재하지 않습니다. (id = " + id + ")");
     }
+
+    public MemberNotFoundException(String username) {
+        super("해당 username의 회원이 존재하지 않습니다. (username = " + username + ")");
+    }
 }
