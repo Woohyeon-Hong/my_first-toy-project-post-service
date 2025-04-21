f537e2a5eedfab0bda2eaf4a562950ac41f6efa8
woohyeon <woohyeon6885@gmail.com>
2025-04-21 19:38:12 +0900

    feat: CommentController API 명세서 수정

--- src/main/java/hong/postService/web/comment/v2/CommentController.java
+++ src/main/java/hong/postService/web/comment/v2/CommentController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Tag(name = "Comment", description = "댓글 관련 API")
 @RestController
 @RequestMapping("/v2/comments")
 @RequiredArgsConstructor
