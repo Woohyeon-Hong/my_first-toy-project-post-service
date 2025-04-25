5cd6ba1a5443cf0d6be5789711317c4fd4cbab8f
woohyeon <woohyeon6885@gmail.com>
2025-04-26 08:15:43 +0900

    feat: CORS에 쿠키 추가

--- src/main/java/hong/postService/config/SecurityConfig.java
+++ src/main/java/hong/postService/config/SecurityConfig.java
@@ -68,6 +68,7 @@ public class SecurityConfig {
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
 
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
                         configuration.setExposedHeaders(Collections.singletonList("Authorization"));
 
                         return configuration;
