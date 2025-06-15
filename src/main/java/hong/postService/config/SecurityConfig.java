b4bc5463b11da9e55707675ce1d3e857813bbb5a
woohyeon <woohyeon6885@gmail.com>
2025-06-15 12:11:52 +0900

    feat: Spring Boot Actuator의 health check 기능 사용

--- src/main/java/hong/postService/config/SecurityConfig.java
+++ src/main/java/hong/postService/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html",
-                        "/health"
+                        "/actuator/**"
                 ).permitAll()
 
                 // 회원가입, 로그인
