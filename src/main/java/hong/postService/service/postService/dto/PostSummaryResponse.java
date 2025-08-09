244a3f3f3678e375b0604efdd2ca21f86634d464
woohyeon <woohyeon6885@gmail.com>
2025-08-09 15:17:44 +0900

    feat: PostSummaryResponse 생성자 추가

--- src/main/java/hong/postService/service/postService/dto/PostSummaryResponse.java
+++ src/main/java/hong/postService/service/postService/dto/PostSummaryResponse.java
@@ -3,6 +3,7 @@ package hong.postService.service.postService.dto;
 import hong.postService.domain.Post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
@@ -16,11 +17,27 @@ public class PostSummaryResponse {
     private LocalDateTime createdDate;
     private int commentCount;
 
+    private boolean includingFile;
+
+    public PostSummaryResponse(Long id, String title, String writerNickname,
+                               LocalDateTime createdDate, Long commentCount, Boolean includingFile) {
+        this.id = id;
+        this.title = title;
+        this.writerNickname = writerNickname;
+        this.createdDate = createdDate;
+        this.commentCount = commentCount == null ? 0 : commentCount.intValue();
+        this.includingFile = includingFile != null && includingFile;
+    }
+
     public static PostSummaryResponse from(Post post) {
+
+
         return new PostSummaryResponse(post.getId(),
                 post.getTitle(),
                 post.getWriter().getNickname(),
                 post.getCreatedDate(),
-                post.getComments().size());
+                post.getComments().size(),
+                false
+        );
     }
 }
