51c351fe0ce335d8721044acb48fefeeed0eff9b
woohyeon <woohyeon6885@gmail.com>
2025-08-13 20:48:58 +0900

    feat: FileResponse 추가

---
+++ src/main/java/hong/postService/service/fileService/dto/FileResponse.java
@@ -0,0 +1,14 @@
+package hong.postService.service.fileService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileResponse {
+
+    private Long id;
+    private String originalFileName;
+}
