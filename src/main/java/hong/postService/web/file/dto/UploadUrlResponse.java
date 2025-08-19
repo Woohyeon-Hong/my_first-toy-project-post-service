f83b163914589d5835e89bd35939d27c7663fa72
woohyeon <woohyeon6885@gmail.com>
2025-08-19 10:21:18 +0900

    feat: UploadUrlResponse 추가

---
+++ src/main/java/hong/postService/web/file/dto/UploadUrlResponse.java
@@ -0,0 +1,26 @@
+package hong.postService.web.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UploadUrlResponse {
+
+    List<Item> results;
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class Item{
+        private String originalFileName;
+        private String s3Key;
+        private String storedFileName;
+        private String uploadUrl;
+        private final Instant expiresAt;
+    }
+}
+
