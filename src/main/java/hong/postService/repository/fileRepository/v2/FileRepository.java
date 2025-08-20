ceaf8e191a6187796b1c83237055dc64708cfb08
woohyeon <woohyeon6885@gmail.com>
2025-08-20 15:11:06 +0900

    feat: FileRepository에 findByIsRemovedTrueAndRemovedBefore() 추가

--- src/main/java/hong/postService/repository/fileRepository/v2/FileRepository.java
+++ src/main/java/hong/postService/repository/fileRepository/v2/FileRepository.java
@@ -2,9 +2,12 @@ package hong.postService.repository.fileRepository.v2;
 
 import hong.postService.domain.File;
 import hong.postService.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,4 +36,6 @@ public interface FileRepository extends JpaRepository<File, Long> {
 
     @Query("select f from File f left join fetch f.post where f.post = :post and f.isRemoved = false")
     List<File> findAllByPostIdAndIsRemovedFalse(Post post);
+
+    Page<File> findByIsRemovedTrueAndRemovedBefore(LocalDateTime threshold, Pageable pageable);
 }
